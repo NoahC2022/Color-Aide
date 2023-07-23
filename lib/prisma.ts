6ca569373a93a35900f0b5ac5381d8014eee192a
Crawford Leeds <crawford@crawfordleeds.com>
2023-07-23 14:37:05 -0600

    fix import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../prisma/__generated__/prisma-client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
