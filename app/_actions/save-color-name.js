b43173e989dce8018ab3eb75deac6fd4f1deb9da
Crawford Leeds <crawford@crawfordleeds.com>
2023-07-23 13:58:30 -0600

    persist to db

--- app/_actions/save-color-name.js
+++ app/_actions/save-color-name.js
@@ -1,8 +1,14 @@
-"use server"
+"use server";
 
+import { prisma } from "@/lib/prisma";
 export const saveColorName = async (colorHex, name) => {
-    console.log('color hex: ', colorHex)
-    console.log('name: ', name)
+  console.log("color hex: ", colorHex);
+  console.log("name: ", name);
 
-
-}
+  await prisma.colorName.create({
+    data: {
+      hex: colorHex,
+      name,
+    },
+  });
+};
