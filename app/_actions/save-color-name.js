75f049f65ae2c2546b08c0429482aa01e6770261
Crawford Leeds <crawford@crawfordleeds.com>
2023-07-23 14:00:23 -0600

    move generated output

--- app/_actions/save-color-name.js
+++ app/_actions/save-color-name.js
@@ -5,10 +5,11 @@ export const saveColorName = async (colorHex, name) => {
   console.log("color hex: ", colorHex);
   console.log("name: ", name);
 
-  await prisma.colorName.create({
+  const record = await prisma.colorName.create({
     data: {
       hex: colorHex,
       name,
     },
   });
+  console.log("saved color name: ", record);
 };
